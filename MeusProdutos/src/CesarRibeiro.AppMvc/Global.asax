18fa67807802045b0ba0f478113afa33ea3f187d
CesarRibeiro <cesarribeiro@outlook.com>
2022-01-23 18:16:11 -0300

    subindo o projeto

---
+++ MeusProdutos/src/CesarRibeiro.AppMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CesarRibeiro.AppMvc.MvcApplication" Language="C#" %>
---
+++ MeusProdutos/src/CesarRibeiro.AppMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CesarRibeiro.AppMvc.Startup))]
+namespace CesarRibeiro.AppMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
